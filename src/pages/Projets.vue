03d9c3536ddd9df769c98ffa90f154d17ac6fefb
Fred <frederic@ichisar.com>
2020-03-29 14:50:53 +0200

    Create projects page, fix authors

---
+++ src/pages/Projets.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="hero">
+          <h1 class="hero-title">Projets</h1>
+      </div>
+      <ProjectsGrid :projects="$page.projects.edges" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Posts {
+	projects: allProjectPost {
+    edges {
+      node {
+        id
+        date (format: "YYYY")
+        title
+        categories
+        thumbnail (quality: 90)
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import ProjectsGrid from "@/components/ProjectsGrid"
+
+export default {
+  components: {
+    ProjectsGrid
+  }
+}
+</script>
